5c23047952126c117d584b495a217fec7f7db11b
Praharsha <chimalapraharsha21@iisertvm.ac.in>
2024-01-12 16:26:09 +0530

    Deleted scrap.py

--- scrap.py
+++
@@ -1,4 +0,0 @@
-import os
-folder = r"D:\Users\HARSHU\Downloads\A database of eight common tomato pest images\A database of eight common tomato pest images\Tomato pest image enhancement\Tomato pest image enhancement"
-
-print(subfolders[0])
