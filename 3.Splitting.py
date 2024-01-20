76cde44be84f525c1b2aec7d6e11352777246e4a
Praharsha <chimalapraharsha21@iisertvm.ac.in>
2024-01-20 11:35:03 +0530

    comments updated

--- 3.Splitting.py
+++ 3.Splitting.py
@@ -3,23 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 # Specify the path to your CSV file
-# csv_file_path = r"D:\Users\HARSHU\Downloads\A database of eight common tomato pest images\A database of eight common tomato pest images\Tomato pest image enhancement\Tomato pest image enhancement\images_data.csv"
-#
-# # Load the CSV file into a Pandas DataFrame
-# df = pd.read_csv(csv_file_path)
-# # #########################################################################
-# # Splitting the data into test and train (80:20)
-#
-# X = df['pixels'].values
-# y = df['pest'].values
-# X = np.array([np.fromstring(pixel, dtype=int, sep=' ') for pixel in X]) # making sure every value is numeric
-# # # Split the data into training and testing sets
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4)
-# # Save X_train, X_test, y_train, y_test as .npy files
-# np.save("D:\Projects\Tomato_pest_classification/X_train.npy", X_train)
-# np.save("D:\Projects\Tomato_pest_classification/X_test.npy", X_test)
-# np.save("D:\Projects\Tomato_pest_classification/y_train.npy", y_train)
-# np.save("D:\Projects\Tomato_pest_classification/y_test.npy", y_test)
+csv_file_path = r"D:\Users\HARSHU\Downloads\A database of eight common tomato pest images\A database of eight common tomato pest images\Tomato pest image enhancement\Tomato pest image enhancement\images_data1.csv"
+
+# Load the CSV file into a Pandas DataFrame
+df = pd.read_csv(csv_file_path)
+# #########################################################################
+# Splitting the data into test and train (80:20)
+
+X = df['pixels'].values
+y = df['pest'].values
+X = np.array([np.fromstring(pixel, dtype=int, sep=' ') for pixel in X]) # making sure every value is numeric
+# # Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4)
+# Save X_train, X_test, y_train, y_test as .npy files
+np.save("D:\Projects\Tomato_pest_classification/X_train.npy", X_train)
+np.save("D:\Projects\Tomato_pest_classification/X_test.npy", X_test)
+np.save("D:\Projects\Tomato_pest_classification/y_train.npy", y_train)
+np.save("D:\Projects\Tomato_pest_classification/y_test.npy", y_test)
 
 # loading the numpy files
 xtrain = np.load("D:\Projects\Tomato_pest_classification/X_train.npy")
